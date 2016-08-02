cc439ee440d7187c4fb93bfb57f46eebb7bcc1d5
Praveenmail2him <Praveenmail2him@gmail.com>
2016-08-02 15:37:39 +0530

    Add files via upload

---
+++ src/test/java/com/infosys/km/openshiftdemo/OpenshiftKmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.km.openshiftdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenshiftKmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
